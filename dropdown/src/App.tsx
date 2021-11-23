95a6bbe1d2e8cde9610d3476cd616088a77b654f
설영환 <seol-yeonghwan@seol-yeonghwan-ui-MacBookPro-2.local>
2021-11-23 17:42:05 +0900

    [feat] drag& drop fin

--- dropdown/src/App.tsx
+++ dropdown/src/App.tsx
@@ -1,39 +1,40 @@
 import React, { FormEvent, useRef, useState } from "react";
 
 const App: React.FC = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
+  const [inputValue, setInputValue] = useState("");
   const [dragging, setDragging] = useState<number | null>(null);
   const [squareDates, setSquareDatas] = useState<{ color: string }[]>([]);
   const colorBoxRef = useRef<{ color: string }[]>(squareDates);
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log(inputRef.current?.value);
-    if (inputRef.current?.value !== "") {
-      setSquareDatas((prev) => [
-        ...prev,
-        { color: inputRef.current?.value || "black" },
-      ]);
+    if (inputValue !== "") {
+      setSquareDatas((prev) => [...prev, { color: inputValue }]);
+      setInputValue("");
     } else {
       alert("색을 입력해주세요");
     }
   };
   const handleDrapStart = (idx: number, e: React.DragEvent<HTMLDivElement>) => {
-    console.log(e);
     setDragging(idx);
   };
   const handleDragEnter = (
     idx: number,
-    event: React.DragEvent<HTMLDivElement>,
+    event: React.DragEvent<HTMLDivElement>
   ) => {
     setSquareDatas((prev) => {
       colorBoxRef.current = prev;
       if (typeof dragging === "number" && idx < dragging)
         return [
           ...prev.slice(0, idx),
-          prev[dragging],
-          ...prev.slice(idx, dragging),
           { color: "transparent" },
-          ...prev.slice(dragging + 1),
+          ...prev.slice(idx),
+        ];
+      if (typeof dragging === "number" && idx > dragging)
+        return [
+          ...prev.slice(0, idx - 1),
+          { color: "transparent" },
+          ...prev.slice(idx - 1),
         ];
       return prev;
     });
@@ -42,22 +43,29 @@ const App: React.FC = () => {
   const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
     setSquareDatas([...colorBoxRef.current]);
   };
-  const handleDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
+  const handledrop = (
+    event: React.DragEvent<HTMLDivElement>,
+    thisIdx: number
+  ) => {
     // setSquareDatas([...colorBoxRef.current]);
+    const changeColor = squareDates[(dragging as number) + 1];
     const newSquare = squareDates.filter(
-      (_, idx) => idx !== (typeof dragging === "number" ? dragging : 0) + 1,
+      (_, idx) => idx !== (typeof dragging === "number" ? dragging : 0) + 1
     );
-    console.log(squareDates);
-    console.log(newSquare);
+    newSquare[thisIdx] = changeColor;
     colorBoxRef.current = newSquare;
     setSquareDatas(newSquare);
     setDragging(null);
   };
-  console.log(dragging);
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" ref={inputRef} placeholder="색을 입력해주세요" />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="색을 입력해주세요"
+        />
       </form>
 
       <section style={{ marginTop: "20px" }}>
@@ -72,7 +80,12 @@ const App: React.FC = () => {
               marginRight: "20px",
             }}
             onDragStart={(e) => handleDrapStart(idx, e)}
-            onDragEnd={handleDragEnd}
+            onDragOver={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+            }}
+            onDrop={(e) => handledrop(e, idx)}
+            // onDragEnd={handleDragEnd}
             onDragEnter={(e) => handleDragEnter(idx, e)}
             onDragLeave={handleDragLeave}
           ></div>
